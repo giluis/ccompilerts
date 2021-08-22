3efd0a7667c67c9decf604e0c1f92d39eb6e7efe
macholatino45 <lupevibogil@gmail.com>
2021-08-22 12:15:11 +0100

    add parse function and Program term

--- parser3.ts
+++ parser3.ts
@@ -1,3 +1,19 @@
+import { Statement } from "./grammar.ts";
 import { Token, TokenKind } from "./lexer.ts";
 import { OperationPriorities } from "./operations.ts";
+import ParsingResult from "./parsingResult.ts";
+
+export default function parse(tokens:Token[]):string | object{
+    const [, r_ast ] = Program(0,tokens);
+    if (r_ast.isFailure){
+        return r_ast.failureMsg;
+    } else {
+        return r_ast.build();
+    }
+}
+
+export function Program(pointer:number, tokens:Token[]):[number, ParsingResult ]{
+
+    return Statement(pointer, tokens);
+}
 
