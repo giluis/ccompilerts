121cb75536aa94fc13c01d23ffe41bfbd2f5bd29
macholatino45 <lupevibogil@gmail.com>
2021-08-22 22:37:10 +0100

    add tests for functiondef and return stat

--- tests/grammar.test.ts
+++ tests/grammar.test.ts
@@ -1,7 +1,59 @@
-import { SingleToken, Args, Variable } from "../grammar.ts";
-import lex, { Token, TokenKind } from "../lexer.ts";
-import { assert, equal } from "../deps.ts";
-import ParsingResult from "../parsingResult.ts";
+import { SingleToken, Args, Variable, Identifier, ReturnStatement, FunctionDef, Statement } from "../src/grammar.ts";
+import lex, { Token, TokenKind } from "../src/lexer.ts";
+import { assert, assertEquals } from "../deps.ts";
+import ParsingResult from "../src/parsingResult.ts";
+
+testArgs();
+testFunctionDefNoArgs();
+
+function testFunctionDefNoArgs(){
+    let tokens = lex("int main(){return 2;}");
+    const [ptr1, r_functionDef] = FunctionDef(0,tokens);
+    assert(!r_functionDef.isFailure);
+    let functionDefBuilt = r_functionDef.build();
+    const expectedArgs = Args(2,tokens)[1].build();
+    const expectedStatements = [Statement(0,lex("return 2;"))[1].build()];
+    const expected = {
+        identifier: "main",
+        returnType: TokenKind.Kw_Int,
+        body: expectedStatements,
+        args: expectedArgs,
+    }
+
+    assertEquals(functionDefBuilt, expected);
+}
+function testFunctionDef(){
+    let tokens = lex("int add(int arg1,int arg2){return 2;}");
+    const [ptr1, r_functionDef] = FunctionDef(0,tokens);
+    assert(!r_functionDef.isFailure);
+    let functionDefBuilt = r_functionDef.build();
+    const expectedArgs = Args(2,tokens)[1].build();
+    const expectedStatements = [Statement(0,lex("return 2;"))[1].build()];
+    const expected = {
+        identifier: "add",
+        returnType: TokenKind.Kw_Int,
+        body: expectedStatements,
+        args: expectedArgs,
+    }
+
+    assertEquals(functionDefBuilt, expected);
+}
+function testReturnStatement() {
+    let tokens = lex("return 2;");
+    const [ptr1, r_returnStatement] = ReturnStatement(0, tokens);
+
+    assert(!r_returnStatement.isFailure);
+
+    let returnStatementBuiltl = r_returnStatement.build();
+
+    assertEquals(returnStatementBuiltl, {
+        statementType: TokenKind.Kw_Return,
+        returnValue: {
+            kind: TokenKind.Lit_Int,
+            value: "2",
+        },
+    });
+}
 
 testVariable();
 
@@ -14,48 +66,41 @@ function testVariable() {
     const [isFailure, failureMsg] = ParsingResult.and(r_var1, r_var2, r_var3);
     assert(!isFailure && failureMsg === "");
 
-    let r_var1Built = r_var1.build()
-    let r_var2Built = r_var2.build()
-    let r_var3Built = r_var3.build()
+    let r_var1Built = r_var1.build();
+    let r_var2Built = r_var2.build();
+    let r_var3Built = r_var3.build();
 
-    equal(r_var1Built, {
+    assertEquals(r_var1Built, {
         type: TokenKind.Kw_Int,
         identifier: "arg1",
     });
-    equal(r_var2Built, {
+    assertEquals(r_var2Built, {
         type: TokenKind.Kw_Int,
         identifier: "arg2",
     });
-    equal(r_var3Built, {
+    assertEquals(r_var3Built, {
         type: TokenKind.Kw_Int,
         identifier: "arg3",
     });
 }
+
 function testArgs() {
     let tokens = lex("somefunc(int arg1,int arg2 , int arg3 )");
     const [newptr, r_args] = Args(1, tokens);
     assert(!r_args.isFailure);
-    let argsBuilt = r_args.build();
-    equal(argsBuilt, [
-        {
-            type: TokenKind.Kw_Int,
-            identifier: "arg1",
-        },
-        {
-            type: TokenKind.Kw_Int,
-            identifier: "arg2",
-        },
-        {
-            type: TokenKind.Kw_Int,
-            identifier: "arg3",
-        },
-    ]);
+    let argsBuilt: any[] = r_args.build();
+    argsBuilt.forEach((arg, i) => {
+        assert(arg.type === TokenKind.Kw_Int);
+        assert(arg.identifier === `arg${i + 1}`);
+    });
+
+    assert(newptr === 11);
 }
 
 function testSingleToken() {
     let tokens = lex("a = 0");
-    let [ptr1, r_identifier] = SingleToken(TokenKind.Identifier, 0, tokens);
-    let [ptr2, r_equals] = SingleToken(TokenKind.Assign, ptr1, tokens);
+    let [ptr1, r_identifier] = Identifier(0, tokens);
+    let [ptr2, r_equalSign] = SingleToken(TokenKind.Assign, ptr1, tokens);
     let [ptr3, r_litInt] = SingleToken(TokenKind.Lit_Int, ptr2, tokens);
 
     let arePtrsSequential = ptr3 === ptr2 + 1 && ptr2 + 1 === ptr1 + 2;
@@ -63,7 +108,7 @@ function testSingleToken() {
 
     let [isFailure, failureMsg] = ParsingResult.and(
         r_identifier,
-        r_equals,
+        r_equalSign,
         r_litInt,
     );
     assert(!isFailure, "Should be false ");
@@ -73,12 +118,12 @@ function testSingleToken() {
     );
 
     let identifierBuilt = r_identifier.build();
-    let equalsBuilt = r_equals.build();
+    let equalSignBuilt = r_equalSign.build();
     let litIntBuilt = r_litInt.build();
 
-    equal(identifierBuilt, { kind: TokenKind.Identifier, value: "a" });
-    equal(equalsBuilt, { kind: TokenKind.Assign, b: 2 });
-    equal(identifierBuilt, { kind: TokenKind.Identifier, value: "a" });
+    assertEquals(identifierBuilt, "a");
+    assertEquals(equalSignBuilt, TokenKind.Assign);
+    assertEquals(litIntBuilt, { kind: TokenKind.Lit_Int, value: 2 });
 }
 
 testSingleToken();
