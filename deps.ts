6ca4d1ac0626f1a74e8e0cc3086fc3b90cb4aa70
macholatino45 <lupevibogil@gmail.com>
2021-08-22 22:34:56 +0100

    add assertEquals

--- deps.ts
+++ deps.ts
@@ -1,7 +1,8 @@
-import { assert,equal } from "https://deno.land/std@0.104.0/testing/asserts.ts";
+import { assert,equal,assertEquals } from "https://deno.land/std@0.104.0/testing/asserts.ts";
 
 export {
     equal,
-    assert
+    assert,
+    assertEquals,
 }
 const o = {attr1: "something", attr2: "something else"}
