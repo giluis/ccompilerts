605c6be4af795dcbdecd8ca215808c59ebe5093e
macholatino45 <lupevibogil@gmail.com>
2021-08-22 12:15:36 +0100

    add equal assertion to deps

--- deps.ts
+++ deps.ts
@@ -1,5 +1,7 @@
-import { assert } from "https://deno.land/std@0.104.0/testing/asserts.ts";
+import { assert,equal } from "https://deno.land/std@0.104.0/testing/asserts.ts";
 
 export {
+    equal,
     assert
 }
+const o = {attr1: "something", attr2: "something else"}
